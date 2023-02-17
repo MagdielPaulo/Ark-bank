98061e4e5f559643f3e5a9f3951c4c792c06d984
Erlaniel <Erlaniel@DKTGEITI12>
2023-02-17 10:26:53 -0300

    Commit conseguiuRetirar

--- Ark-bank-main/arkbank/src/Conta.java
+++ Ark-bank-main/arkbank/src/Conta.java
@@ -2,24 +2,19 @@ package arkbank.src;
 
 public class Conta {
 	
-	// Atributo que representa o saldo disponível na conta.
 	double saldo;
-	// Atributo que representa o número da agência bancária associada à conta.
 	int agencia;	
-	// Atributo que representa o número da conta.
 	int numero;	
-	// Atributo que representa o nome do titular da conta.
 	String titular;
 	
-	//Método criado
+	
 	public void deposita(double valor) {
-		// Adiciona o valor passado como parâmetro ao saldo atual da conta
-		this.saldo = this.saldo + valor;
+		this.saldo += valor;
 	}
 	
 	public boolean saca(double valor) {
 		if (this.saldo >= valor) {
-			this.saldo = this.saldo - valor;
+			this.saldo -= valor;
 			return true;
 		}else {
 			return false;
--- Ark-bank-main/arkbank/src/TestaMetodo.java
+++ Ark-bank-main/arkbank/src/TestaMetodo.java
@@ -2,14 +2,14 @@ package arkbank.src;
 
 public class TestaMetodo {
 	public static void main(String[] args) {
-		// Cria uma instância da classe Conta e atribui o valor 100 ao seu saldo
 		Conta contaBank = new Conta();
 		contaBank.saldo = 100;
-		
-		// Chama o método deposita passando o valor 20 como parâmetro
 		contaBank.deposita(50);
-		
-		// Imprime o saldo da conta após o depósito
 		System.out.println(contaBank.saldo);
+		
+		
+		boolean conseguiuRetirar = contaBank.saca(20);
+		System.out.println(contaBank.saldo);
+		System.out.println(conseguiuRetirar);
 	}
 }
